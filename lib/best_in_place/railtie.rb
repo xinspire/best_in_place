b51abcf0921c296870bcd60c053d994ca88eab0c
Mickey Sheu <mcsheu@gmail.com>
2020-01-28 09:16:44 -0600

    MOD: fix deprecation notice initializing ActionView::Base.new

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -4,7 +4,7 @@ require 'action_view/base'
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new
+      BestInPlace::ViewHelpers = ActionView::Base.new(ActionController::Base.view_paths, {}, nil)
     end
   end
 end
